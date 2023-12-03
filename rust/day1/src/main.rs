816bb42e344568592492f89608292e14d449208c
Thim Lohse <thimdev@gmail.com>
2023-12-03 15:13:04 +0100

    trying regex

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,3 +1,20 @@
+use regex::Regex;
+use std::fs;
+
+fn decodeLine(line: &str) {
+    let re = Regex::new(r"(\d){1}").unwrap();
+    let digits = re.captures(line).map(String::from);
+
+    println!("{digits}")
+}
 fn main() {
-    println!("Hello, world!");
+    let file_path = "assets/puzzle.txt";
+    // --snip--
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    for line in contents.split("\n") {
+        println!("{line}")
+    }
 }
