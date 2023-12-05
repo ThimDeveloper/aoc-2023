b09c17802a61b534172bbe98b5383184a91b0bb3
Thim Lohse <thimdev@gmail.com>
2023-12-05 09:32:03 +0100

    matrix conversion

--- python/src/aoc/day3/enginefixer.py
+++ python/src/aoc/day3/enginefixer.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from typing import List
 from aoc.shared.io import FileUtility
 import regex as re
 
@@ -10,13 +11,18 @@ class EngineFixer(FileUtility):
         output_file_name: str = "output.txt",
     ) -> None:
         super().__init__(__file__, assets_directory, output_file_name)
-        self.symbol_pattern = re.compile(r"(\d+)|(\D+)")
+        self.symbol_pattern = re.compile(r"(\d+)|(\D){1}")
 
     def sum_of_parts(self, file_name: str) -> int:
         file_path = self._get_file_path(file_name)
         with open(file_path, "r") as fd:
             lines = [line.replace("\n", "") for line in fd.readlines()]
-            symbol_matrix = [self.symbol_pattern.findall(line) for line in lines]
-            print("\n")
+            symbol_matrix = []
+            for idx_row, row in enumerate(lines):
+                symbol_matrix.append([])
+                for column_value in self.symbol_pattern.findall(row):
+                    filtered_column_value = list(filter(None, column_value))[0]
+                    symbol_matrix[idx_row].append(filtered_column_value)
+            # [x for y in collection for x in y] # [A for B in C for D in E]
             pprint(symbol_matrix)
             return symbol_matrix
